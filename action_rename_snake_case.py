3240030a4ca6263402a947be86c0f38f6a36056c
nousea@deplatine <nousea@deplatine>
2019-06-06 16:57:33 +0200

    adding new action: rename_snake_case

---
+++ action_rename_snake_case.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+#####################################################################
+#####################################################################
+## @author: Naoual ASKMI
+## @description: Nemo Action: 
+##   rename file or directory by using snake_case naming conventions
+##
+##   JoJo no Kimyou na Bouken - Ougon no Kaze - 32
+##   -->
+##   jojo_no_kimyou_na_bouken_-_ougon_no_kaze_-_32
+##
+#####################################################################
+#####################################################################
+
+import os
+import sys
+import time
+
+LOGFILE = '/tmp/snake_case_renaming.log'
+
+with open(LOGFILE, 'a+') as logfile:
+  now = time.asctime(time.localtime(time.time()))
+
+  for file_path in sys.argv[1:]:
+    # extract file name and dir path from args.
+    file_name = file_path.split("/")[-1]
+    dir_path = '/'.join(file_path.split("/")[:-1])
+    # construct new names.
+    new_name = file_name.lower().replace(' ', '_')
+    before, after = dir_path+'/'+file_name, dir_path+'/'+new_name
+    # renaming process.
+    os.rename(before, after)
+    logfile.write('{} -:- {} -> {}\n'.format(now, before, after))
